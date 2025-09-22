45d384ee881e7b091140699929e4faa9a7a2fdda
revaztutarashvili <generationofjeans@gmail.com>
2025-09-22 23:10:06 +0400

    add base project

---
+++ src/main/java/com/example/E_commerce/Ecommerce8Application.java
@@ -0,0 +1,13 @@
+package com.example.E_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce8Application.class, args);
+	}
+
+}
