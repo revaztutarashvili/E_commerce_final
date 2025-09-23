27e40871e3a893ee0eb15d56296b6304927f6cdc
revaztutarashvili <generationofjeans@gmail.com>
2025-09-24 03:48:47 +0400

    add swagger problem solve. @Hidden annotation was necesserry in global exception class, under @@RestControllerAdvice

--- src/main/java/com/nabiji/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nabiji/ecommerce/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 // 📂 src/main/java/com/nabiji/ecommerce/exception/GlobalExceptionHandler.java
 package com.nabiji.ecommerce.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
